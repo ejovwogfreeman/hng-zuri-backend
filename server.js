7b0ef9e5c63a61a3c0991ebd342bd6c49abae9df
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-11-01 16:29:43 +0100

    message

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const port = 8000;
+
+app.get("/api", (req, res) => {
+  res.send({
+    slackUsername: "Ejovwo Godbless",
+    backend: true,
+    age: 23,
+    bio: "Hello there, I am Ejovwo Godbless and I am currently learning backend web development at Zuri Internship HNG9",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
